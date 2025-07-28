ea267e4cf3882f6944d033459727434ad37deace
leesu9042 <tnckddl8957@gmail.com>
2025-07-28 16:13:23 +0900

    refacto : circle 중심 좌표 center propoerty 추가

--- src/util/createCirclePolygon.js
+++ src/util/createCirclePolygon.js
@@ -1,8 +1,10 @@
 import * as turf from "@turf/turf";
 
-
+// 태풍 폴리곤 생성 1단계 circle 생성
 // geoJson에서 featurecollcetion을 가져와서 그안의 property명을 반지름삼아
 // circle polygon type의 Geojson반환
+
+
 export function createCirclePolygons(data, property, ruler) {
     // reduce를 사용해 이전 지점 정보와 결과 컬렉션을 함께 관리
     const collections = data.features.reduce( //1번질문 objcet는 그냥api로 받아온 data가아니라 별도의 objcet로 파싱한거인지?
@@ -33,13 +35,19 @@ export function createCirclePolygons(data, property, ruler) {
                 const circle = turf.circle(currentPoint, radius, {
                     steps: 64,
                     units: "kilometers",
+                    // 기존 properties 유지 + center 추가
+                    properties : {
+                        ...(item.properties || {}),  // 기존 속성 복사 (없으면 빈 객체)
+                        center: currentPoint,              // center 좌표 추가
+                    }
                 });
 
                 // 원본 properties 유지 (옵션)???
 
-                circle.properties = { ...item.properties };
 
                 acc.push(circle);
+                console.log("circle.properties before:", circle.properties);
+
             } else {
                 //  반경이 0이거나 유효하지 않으면 Point 그대로 사용
                 acc.push(item);
@@ -52,8 +60,6 @@ export function createCirclePolygons(data, property, ruler) {
         }, []);  // 초기값: 결과 빈배열
 
     // reduce 완료 후, 생성된 폴리곤 컬렉션 배열 반환
-    return {
-        type: 'FeatureCollection',
-        features: collections
-    };
+    return turf.featureCollection(collections);
+
 }
