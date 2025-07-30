9c8a1cee793828e278d55404c4b41eee3a3a2eae
leesu9042 <tnckddl8957@gmail.com>
2025-07-30 16:45:09 +0900

    feature : placeholder 기능 추가

--- src/shared/components/dropdown/createDropdown.js
+++ src/shared/components/dropdown/createDropdown.js
@@ -6,11 +6,20 @@
  * @param {string} [options.className] - 클래스 이름 (기본: "cesium-button")
  * @returns {HTMLSelectElement}
  */
-export function createDropdown({ id, items, className = "cesium-button" }) {
+export function createDropdown({ id, items, className = "cesium-button" , placeholder = false} ) {
 
     const select = document.createElement("select");
     select.id = id; //인자로 들어온 id값으로 select를 만든다
     select.className = className; //이건 만들 class이름
+
+
+    if (placeholder) {
+        const defaultOption = document.createElement("option");
+        defaultOption.textContent = placeholder;
+        defaultOption.disabled = true;
+        defaultOption.selected = true;
+        select.appendChild(defaultOption);
+    }
 
     for (const item of items) { //이건 인자로 받는 dropdown에 올릴 데이터 배열
 
