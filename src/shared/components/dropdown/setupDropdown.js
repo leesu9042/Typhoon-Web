a49a159340698c0f110f1f5846f7c9c349542b92
leesu9042 <tnckddl8957@gmail.com>
2025-07-30 16:13:21 +0900

    feat: 드롭다운 생성 및 이벤트 바인딩 유틸 setupDropdown 추가

---
+++ src/shared/components/dropdown/setupDropdown.js
@@ -0,0 +1,18 @@
+import { createDropdown } from "./createDropdown.js";
+
+/**
+ * 드롭다운 셋업 유틸
+ * @param {Object} options
+ * @param {string} options.wrapperId - div ID
+ * @param {string} options.selectId - select ID
+ * @param {Array} options.items - 배열 (string 또는 {value, text})
+ * @param {function(string)} options.onChange - 선택 시 실행 함수
+ */
+export function setupDropdown({ wrapperId, selectId, items, onChange }) {
+    const wrapper = document.getElementById(wrapperId);
+    wrapper.innerHTML = "";
+
+    const select = createDropdown({ id: selectId, items });
+    select.addEventListener("change", () => onChange(select.value));
+    wrapper.appendChild(select);
+}
