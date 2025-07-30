f4caa8f206af3339c0e1d3c00474be6f1defac07
leesu9042 <tnckddl8957@gmail.com>
2025-07-30 16:44:27 +0900

    refactor : mock 데이터 위치 변경

--- src/shared/components/dropdown/injectDropdown.js
+++ src/shared/components/dropdown/injectDropdown.js
@@ -8,11 +8,14 @@ import { createDropdown } from "./createDropdown.js";
  * @param {Array} options.items - 배열 (string 또는 {value, text})
  * @param {function(string)} options.onChange - 선택 시 실행 함수
  */
-export function setupDropdown({ wrapperId, selectId, items, onChange }) {
+export function injectDropdown({ wrapperId, selectId, items, onChange , placeholder = false  }) {
     const wrapper = document.getElementById(wrapperId);
     wrapper.innerHTML = "";
 
-    const select = createDropdown({ id: selectId, items });
+
+
+
+    const select = createDropdown({ id: selectId, items ,placeholder });
     select.addEventListener("change", () => onChange(select.value));
     wrapper.appendChild(select);
 }
