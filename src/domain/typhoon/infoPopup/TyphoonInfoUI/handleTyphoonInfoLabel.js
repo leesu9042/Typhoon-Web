ecddcfcd843a25d2702bcbaaee20e75ddb9b2437
leesu9042 <tnckddl8957@gmail.com>
2025-08-11 13:37:12 +0900

    feat: 태풍 정보 라벨 생성 및 위치 업데이트 기능 추가

---
+++ src/domain/typhoon/infoPopup/TyphoonInfoUI/handleTyphoonInfoLabel.js
@@ -0,0 +1,25 @@
+import {createEntityInfoTemplate} from "./createEntityInfoTemplate.js";
+import {render} from "lit";
+import {updatePosition} from "../utils/updatePostion.js";
+
+export function handleTyphoonInfoLabel(entity, viewer) {
+    const container = document.getElementById("entity-label");
+
+    // 1. 엔티티 정보를 기반으로 html을 만듦
+    const template = createEntityInfoTemplate(entity);
+
+    // 2. 그걸 DOM에 렌더링
+    render(template, container);
+
+    // 3. 카메라가 움직일 때마다 html 위치를 업데이트
+    const updater = () => updatePosition(entity, viewer, container);
+    viewer.scene.postRender.addEventListener(updater);
+
+    // 4. 나중에 제거할 수 있게 함수를 반환
+    return {
+        remove: () => {
+            viewer.scene.postRender.removeEventListener(updater);
+            render(null, container); // info 창 제거
+        },
+    };
+}
