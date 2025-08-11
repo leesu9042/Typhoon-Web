11d052019387cfd762ee3a9c82867e8cc56780a3
leesu9042 <tnckddl8957@gmail.com>
2025-08-11 13:21:34 +0900

    feature : 범례데이터 시각화 기능 관련 코드 추가
    범례데이터 창 html, css
    로드해오는 js함수까지 추가

---
+++ src/domain/typhoon/infoPopup/legend/handleLegendUI.js
@@ -0,0 +1,5 @@
+import {injectLegendHTML} from "./injectLegendHTML.js";
+
+export function handleLegendUI() {
+    injectLegendHTML("legend-container");
+}
---
+++ src/domain/typhoon/infoPopup/legend/injectLegendHTML.js
@@ -0,0 +1,27 @@
+// setupTyphoonEntityLabelHandler함수에서 billbaord(태풍의 point) 선택되면 범례데이터 불러와지게 그렇게 설정되어있음
+
+
+let isInjected = false;
+
+
+export async function injectLegendHTML(containerId, path = '/legend/legend.html') {
+    if (isInjected) return;
+
+    const container = document.getElementById(containerId);
+    if (!container) {
+        console.warn(`[legend] container not found: ${containerId}`);
+        return;
+    }
+
+    try {
+        const res = await fetch(path);
+        if (!res.ok) throw new Error(`Failed to load legend from ${path}`);
+
+        const html = await res.text();
+        container.innerHTML = html;
+        isInjected = true; // ✅ 한 번만 로드하게 됨
+
+    } catch (err) {
+        console.error('[legend] Failed to load:', err);
+    }
+}
