d3fa00a88a5619ed6dfe5aec03253a7f0ac07d8e
leesu9042 <tnckddl8957@gmail.com>
2025-09-24 14:07:39 +0900

    update packages and src

--- src/main.js
+++ src/main.js
@@ -33,90 +33,8 @@ async function main(){
 
 
 
-// // 따라다닐 엔티티 (예: 움직이는 위성)
-//     const myEntity = viewer.entities.add({
-//         name: "움직이는 엔티티",
-//         position: Cesium.Cartesian3.fromDegrees(127.0, 37.5),
-//         point: { pixelSize: 10, color: Cesium.Color.RED }
-//     });
-//     htmlLabel.style.display = "block"; // 표시
-//
-// // 카메라가 렌더될 때마다 엔티티 위치를 추적해서 HTML 위치 갱신
-//     viewer.scene.postRender.addEventListener(() => {
-//         const position = myEntity.position.getValue(Cesium.JulianDate.now());
-//         if (!position) return;
-//
-//         const screenPosition = Cesium.SceneTransforms.worldToWindowCoordinates(viewer.scene, position);
-//         if (!screenPosition) return;
-//
-//         htmlLabel.style.left = `${screenPosition.x}px`;
-//         htmlLabel.style.top = `${screenPosition.y - 40}px`; // 말풍선처럼 위로 띄우기
-//     });
-
-    //
-    // const handler = getClickedBillboard(viewer, function (entity) {
-    //     console.log("클릭한 포인트 엔티티:", entity.properties.values);
-    // });
-
-
-
-    // let activeLabel = null;
-
     // 3 ui들 띄우기 (태풍 info , 범례 html 판)
     setupTyphoonEntityLabelHandler(viewer);
-
-    // 3. 태풍 ui 띄우기
-
-    // // 함수1
-    // //1. 클릭한 billboard entity 객체 가져오기
-    // const handler = getClickedBillboard(viewer, function (entity) {
-    //     console.log("클릭한 포인트 엔티티:", entity.properties?.values);
-    //
-    //     injectLegendHTML('legend-container' );
-    //
-    //
-    //     // 기존 라벨 제거
-    //     if (activeLabel) {
-    //         activeLabel.remove();
-    //         activeLabel = null;
-    //     }
-    //
-    //     //함수 2
-    //     //  라벨 만들기 (entity property가지고 html 템플릿 만들기)
-    //     const template = createEntityInfoTemplate(entity);
-    //
-    //
-    //     // 라벨 표시할 DOM 요소
-    //     const container = document.getElementById('entity-label');
-    //
-    //     // 화면에 렌더링
-    //     render(template, container);
-    //
-    //
-    //     //함수 3
-    //     //  좌표 따라 움직이도록 postRender 등록
-    //     //현재 엔티티의 포지션에 맞게끔 html의 포지션도 바꿔주기
-    //
-    //     //updatePosition 선택된 엔티티 값의 postion에 따라
-    //     // html의 위치도 변경해주는 함수
-    //     // 함수를 변수명으로 정의후
-    //     const positionUpdater = () => updatePosition(entity, viewer, container);
-    //
-    //
-    //
-    //
-    //     // 렌더링 될때마다 실행할 함수로 넣기
-    //     viewer.scene.postRender.addEventListener(positionUpdater);
-    //
-    //     //  나중에 제거할 수 있도록 객체 저장
-    //     activeLabel = {
-    //         remove: () => {
-    //             viewer.scene.postRender.removeEventListener(positionUpdater);
-    //             render(null, container); // 라벨 지우기
-    //         }
-    //     };
-    // });
-    //
 
 
 
@@ -129,13 +47,3 @@ main()
 
 
 
-// // 스위치 가져오는
-// loadSwitchComponent();
-//
-//
-//
-// async function loadSwitchComponent() {
-//     const res = await fetch('/src/domain/typhoon/ui/components/switch.html');
-//     const html = await res.text();
-//     document.getElementById('switchWrapper').innerHTML = html;
-// }
